1db036aac82e09c07d9873507bb4783c0765ac7d
Tyuiu.ZamyatinKP@outlook.com <zamyatin_kirya@bk.ru>
2025-12-09 23:35:24 +0500

    Task4

--- Tyuiu.ZamyatinKP.Sprint5.Task4.V6.Lib/DataService.cs
+++ Tyuiu.ZamyatinKP.Sprint5.Task4.V6.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ZamyatinKP.Sprint5.Task4.V6.Lib
         {
             double x = Convert.ToDouble(File.ReadAllText(path).Replace('.', ','));
 
-            return Math.Round(1 / Math.Cos(x) + Math.Pow(x, 3), 3);
+            return Math.Round(1 / Math.Cos(x) + 2.2 * Math.Pow(x, 2), 3);
         }
     }
 }
